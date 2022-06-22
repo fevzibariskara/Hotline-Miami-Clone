54bae8ae7bfdad3afdfb5069f7469480157a2a86
hexxer <fevofantom@gmail.com>
2022-06-22 19:34:29 +0300

    + refactoring

---
+++ Assets/App/Scripts/Entity/Util/EntityActionController.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityActionController : MonoBehaviour
+{
+    public Action<GameObject> OnAttacked;
+    public Action<Item> OnItemPickedUp, OnItemEquiped;
+    public Action<Vector3> OnMove;
+}
